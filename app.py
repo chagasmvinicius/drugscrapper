f935de68426fdc4cbae95e57ae7ba1661aa0cde1
Vinicius Chagas <vinicius.chagas@g.globo>
2025-05-10 17:27:11 -0300

    Index

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import json
 from flask import Flask, request, Response
+from flask_cors import CORS
 import os
 from scraper import *
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/search', methods=['GET'])
 def search():
